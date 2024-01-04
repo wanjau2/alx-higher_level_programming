cafa335017aa32105b347055fb5d8ed33a9a23e5
wanjau2 <eugenewanjau@gmail.com>
2024-01-05 00:11:24 +0300

    python hello world

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\language thast combines remarkable power with very clear syntax"
+str = str[39:67] + str[107:112] + str[:6]
+print(str)
